f72ece0c5345c5dac9904e86fb0ec639ef492711
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2026-02-21 17:59:14 -0800

    sample rate tweaks

--- src/server/instrument.ts
+++ src/server/instrument.ts
@@ -8,7 +8,8 @@ if (process.env.NODE_ENV !== 'development') {
         integrations: [
             nodeProfilingIntegration()
         ],
-        tracesSampleRate: 0.2,
+        tracesSampleRate: 0.3,
+        sampleRate: 1
         // profileSessionSampleRate: 0.5,
         // profileLifecycle: "trace"
     });
