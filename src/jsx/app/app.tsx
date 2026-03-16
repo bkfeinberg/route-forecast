71787d35341068717d80836e5971db08e0c6d2f6
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2026-03-16 14:52:01 -0700

    Tweak Sentry replay settings

--- src/jsx/app/app.tsx
+++ src/jsx/app/app.tsx
@@ -82,10 +82,10 @@ else {
                 tracePropagationTargets: ["localhost", /^https:\/\/www\.randoplan\.com/],
                 // This sets the sample rate to be 10%. You may want this to be 100% while
                 // in development and sample at a lower rate in production
-                replaysSessionSampleRate: 0.03,
+                replaysSessionSampleRate: 0.01,
                 // If the entire session is not sampled, use the below sample rate to sample
                 // sessions when an error occurs.
-                replaysOnErrorSampleRate: 0.9,
+                replaysOnErrorSampleRate: 0.95,
                 normalizeDepth: 5
             });
         }
